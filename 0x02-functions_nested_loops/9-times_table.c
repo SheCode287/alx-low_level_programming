9cf3d165e4bbc26e118f391ff1497881b02ddea5
SheCode287 <ivy.muthoni.wanjohi@gmail.com>
2022-12-14 14:42:15 -0800

    added a blank line

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,7 @@ void times_table(void)
 	int a;
 	int b;
 	int c;
-	
+
 	for (a = 0; a <= 9; a++)
 	{
 	for (b = 0; b <= 9; b++)
@@ -49,5 +49,4 @@ void times_table(void)
 	}
 	_putchar ('\n');
 	}
-
 }
