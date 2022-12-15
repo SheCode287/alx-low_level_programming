26183fc2c35609a5cc668e5351ae208d62fd7450
SheCode287 <ivy.muthoni.wanjohi@gmail.com>
2022-12-15 09:07:59 -0800

    corrected on spaces and betty

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,39 +1,25 @@
 #include "main.h"
 
 /**
- *
  *  * _isupper - evalue if letter is uppercase .
  *
  *   *@c: print int
  *
  *    * Return: Always 0.
- *
- *     */
-
+ */
 
 
 int _isupper(int c)
 
 {
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
 
-	if (c >= 'A' && c <= 'Z')
-
-	{
-
-		return (1);
-
-	}
-
-
-
-	else
-
-	{
-
-		return (0);
-
-	}
-
-
+else
+{
+return (0);
+}
 
 }
